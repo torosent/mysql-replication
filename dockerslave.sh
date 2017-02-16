4a720a1b4384dc3d1934a9d78cea62ffe82f38de
Tomer Rosenthal <torosent@microsoft.com>
2017-02-16 16:39:22 +0200

    Update dockerslave.sh

--- dockerslave.sh
+++ dockerslave.sh
@@ -6,6 +6,7 @@ apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt-get update
 apt-cache policy docker-engine
 apt-get install -y docker-engine
+docker network create app-tier --driver bridge
 docker run --name mysql-slave --link mysql-master:master \
    --network app-tier \
    -p 3306:3306 \
