600ac1f6727a466f0ad09878c25f2d99aea69526
Tomer Rosenthal <torosent@microsoft.com>
2017-02-16 14:55:27 +0200

    Update dockerslave.sh

--- dockerslave.sh
+++ dockerslave.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update
+apt-get update
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get update
 apt-cache policy docker-engine
-sudo apt-get install -y docker-engine
-sudo systemctl status docker
+apt-get install -y docker-engine
+systemctl status docker
 
- sudo docker run --name mysql-slave --link mysql-master:master \
+ docker run --name mysql-slave --link mysql-master:master \
      --network app-tier \
    -p 3306:3306 \
    -d \
-   -v /datadrive:/bitnami/mysql \
+   -v /datadisks/disk1:/bitnami/mysql \
   -e MYSQL_ROOT_PASSWORD=root_password \
   -e MYSQL_REPLICATION_MODE=slave \
   -e MYSQL_REPLICATION_USER=my_repl_user \
