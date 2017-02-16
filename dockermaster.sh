7ba313805258735201a561276c41055e45f17aba
Tomer Rosenthal <torosent@microsoft.com>
2017-02-16 14:17:49 +0200

    Update dockermaster.sh

--- dockermaster.sh
+++ dockermaster.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update
+apt-get update
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get update
 apt-cache policy docker-engine
-sudo apt-get install -y docker-engine
-sudo systemctl status docker
+apt-get install -y docker-engine
+systemctl status docker
 
 mkdir /datadrive
-sudo docker network create app-tier --driver bridge
-sudo docker run --name mysql-master \
+docker network create app-tier --driver bridge
+docker run --name mysql-master \
    --network app-tier \
    -v /datadrive:/bitnami/mysql \
    -p 3306:3306 \
