1589c5549f672795806c8cc3e2d5a828baeb2b6c
Tomer Rosenthal <torosent@microsoft.com>
2017-02-16 15:49:03 +0200

    Update dockerslave.sh

--- dockerslave.sh
+++ dockerslave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-masterhost=$1
+masterhost="$1"
 apt-get update
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
