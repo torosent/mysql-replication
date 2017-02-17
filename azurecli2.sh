1aab38bf89707d1e3709d8e6687b2a6d5ec89cae
Tomer Rosenthal <torosent@microsoft.com>
2017-02-17 10:15:47 +0200

    Update azurecli2.sh

--- azurecli2.sh
+++ azurecli2.sh
@@ -157,8 +157,8 @@ az vm extension set -n CustomScript --publisher Microsoft.Azure.Extensions \
 	--settings '{"fileUris": ["https://raw.githubusercontent.com/torosent/mysql-replication/master/initattacheddisk.sh"],"commandToExecute": "./initattacheddisk.sh"}'
 
 az vm extension set -n DockerExtension --publisher Microsoft.Azure.Extensions \
-   --vm-name $mastername --resource-group $resourceGroupName \ 
-   --settings '{"docker":{"options": ["-D", "--dns=168.63.129.16"]}}'
+   --vm-name $mastername --resource-group $resourceGroupName \
+   --settings '{"docker":{"options": ["--dns=168.63.129.16"]}}'
    
 az vm extension set -n CustomScript --publisher Microsoft.Azure.Extensions \
 	--version 2.0 --vm-name $mastername --resource-group $resourceGroupName \
@@ -184,8 +184,8 @@ az vm extension set -n CustomScript --publisher Microsoft.Azure.Extensions \
 	--settings '{"fileUris": ["https://raw.githubusercontent.com/torosent/mysql-replication/master/initattacheddisk.sh"],"commandToExecute": "./initattacheddisk.sh"}'
 
 az vm extension set -n DockerExtension --publisher Microsoft.Azure.Extensions \
-   --vm-name $slave1name --resource-group $resourceGroupName \ 
-   --settings '{"docker":{"options": ["-D", "--dns=168.63.129.16"]}}'
+   --vm-name $slave1name --resource-group $resourceGroupName \
+   --settings '{"docker":{"options": ["--dns=168.63.129.16"]}}'
    
 az vm extension set -n CustomScript --publisher Microsoft.Azure.Extensions \
 	--version 2.0 --vm-name $slave1name --resource-group $resourceGroupName \
