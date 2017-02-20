0afbd1770b4210ab54b680d9228b3d95e6924397
Tomer Rosenthal <torosent@microsoft.com>
2017-02-20 11:51:42 +0200

    Update failover.sh

--- failover.sh
+++ failover.sh
@@ -27,3 +27,11 @@ az network nic ip-config update --resource-group $resourceGroupName \
 az network nic ip-config update --resource-group $resourceGroupName \
    --nic-name MySQLSlave1Nic1 --lb-name $loadBalancer --name ipconfig1 \
    --lb-inbound-nat-rules mySQLSSH2 $natRuleMaster 
+
+az vm extension set -n CustomScript --publisher Microsoft.Azure.Extensions \
+	--version 2.0 --vm-name $slave1name --resource-group $resourceGroupName \
+	--settings '{"fileUris": ["https://raw.githubusercontent.com/torosent/mysql-replication/master/dockermaster.sh"],"commandToExecute": "./dockermaster.sh"}'
+
+az vm extension set -n CustomScript --publisher Microsoft.Azure.Extensions \
+	--version 2.0 --vm-name $mastername --resource-group $resourceGroupName \
+	--settings '{"fileUris": ["https://raw.githubusercontent.com/torosent/mysql-replication/master/dockerslave.sh"],"commandToExecute": "./dockerslave.sh '$slave1name'" }'
