3b6840487e6516bcadec45b030f1ddab877cf95a
Tomer Rosenthal <torosent@microsoft.com>
2017-02-20 12:08:21 +0200

    Update failover.sh

--- failover.sh
+++ failover.sh
@@ -9,17 +9,6 @@ declare loadBalancer="mySqlLB"
 declare natRuleMaster="natRuleMaster"
 declare natRuleSlave1="natRuleSlave1"
 
-az network lb inbound-nat-rule delete -g $resourceGroupName --lb-name $loadBalancer -n $natRuleMaster
-az network lb inbound-nat-rule delete -g $resourceGroupName --lb-name $loadBalancer -n $natRuleSlave1
-
-az network lb inbound-nat-rule create --resource-group $resourceGroupName \
-  --lb-name $loadBalancer --name  $natRuleSlave1 --protocol tcp \
-  --frontend-port 3306 --backend-port 3306 --frontend-ip-name myFrontEndPool
-
-az network lb inbound-nat-rule create --resource-group $resourceGroupName \
-  --lb-name $loadBalancer --name $natRuleMaster  --protocol tcp \
-  --frontend-port 3307 --backend-port 3306 --frontend-ip-name myFrontEndPool
-
 az network nic ip-config update --resource-group $resourceGroupName \
    --nic-name MySQLMasterNic1 --lb-name $loadBalancer --name ipconfig1 \
    --lb-inbound-nat-rules mySQLSSH1 $natRuleSlave1
