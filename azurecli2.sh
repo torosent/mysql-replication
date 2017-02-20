c3198fdd3976e225fb854aadef93f3b7c9bc68ee
Tomer Rosenthal <torosent@microsoft.com>
2017-02-20 14:08:33 +0200

    Update azurecli2.sh

--- azurecli2.sh
+++ azurecli2.sh
@@ -50,7 +50,7 @@ echo "Starting deployment..."
 az network vnet create --resource-group $resourceGroupName --location $resourceGroupLocation --name myVnet \
   --address-prefix $vnetprefix  --subnet-name mySubnet --subnet-prefix $subnetprefix
   
-az vm availability-set create --resource-group $resourceGroupName --location $resourceGroupLocation --name mySqlAvailabilitySet --platform-fault-domain-count 3
+az vm availability-set create --resource-group $resourceGroupName --location $resourceGroupLocation --name mySqlAvailabilitySet --platform-update-domain-count 5 --platform-fault-domain-count 3
   
 az network lb create --resource-group $resourceGroupName --location $resourceGroupLocation \
   --name $loadBalancer --public-ip-address myPublicIP \
