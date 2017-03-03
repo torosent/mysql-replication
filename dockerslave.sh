1e959b6b70ac2f9b0bf326c90a9e105593a9f28c
Tomer Rosenthal <torosent@microsoft.com>
2017-03-03 09:22:43 +0200

    Update dockerslave.sh

--- dockerslave.sh
+++ dockerslave.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 masterhost="$1"
 docker rm $(docker ps -a -q) -f
-docker run --name mysql-slave \
-   -p 3306:3306 \
-   --dns 168.63.129.16 \
-   -d \
-   -v /datadisks/disk1:/bitnami/mysql \
+docker run -d -p 3306:3306 \
+  -v /datadisks/disk1:/var/lib/mysql \
+  --name mysql_slave \
+  --dns 168.63.129.16 \
+  -e MASTER_HOST=$masterhost \
+  -e MASTER_PORT=3306 \
   -e MYSQL_ROOT_PASSWORD=root_password \
-  -e MYSQL_REPLICATION_MODE=slave \
-  -e MYSQL_REPLICATION_USER=my_repl_user \
-  -e MYSQL_REPLICATION_PASSWORD=my_repl_password \
-  -e MYSQL_MASTER_HOST=$masterhost \
-  -e MYSQL_MASTER_USER=my_user \
-  -e MYSQL_MASTER_PASSWORD=my_password \
   -e MYSQL_USER=my_user \
   -e MYSQL_PASSWORD=my_password \
-  -e MYSQL_DATABASE=my_database \
-  bitnami/mysql:latest
+  torosent/mysql
