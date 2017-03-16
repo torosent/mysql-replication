773616d37b2b97b04dc5500b7609483c93a88bb6
Tomer Rosenthal <torosent@microsoft.com>
2017-03-16 15:23:43 +0200

    Update dockerslave.sh

--- dockerslave.sh
+++ dockerslave.sh
@@ -2,7 +2,7 @@
 masterhost="$1"
 docker rm $(docker ps -a -q) -f
 docker run -d \
-  -v /datadisks/disk1:/var/lib/mysql \
+  -v /datadisks/disk1/db:/var/lib/mysql \
   --name mysql_slave \
   --net=host \
   -e MASTER_HOST=$masterhost \
